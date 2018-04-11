89dc6a7f9a820f1ef1d763807de04d3eaed4da8e
Droid <mailtosonumadhav@gmail.com>
2018-04-11 16:48:58 +0530

    c

--- app/src/main/java/com/madhav/com/ipl2018/constants/iplUrls/IPLUrls.java
+++ app/src/main/java/com/madhav/com/ipl2018/constants/iplUrls/IPLUrls.java
@@ -12,24 +12,7 @@ public class IPLUrls {
     public static final String TABLE = "groupStandings.js";
     public static final String SQUAD = "squads.js";
 
-    public static final String POLL = "pools.js";
-    public static final String MOST_RUNS = "mostRuns.js";
-    public static final String MOST_SIXES = "mostSixes.js";
-    public static final String HIGHEST_SCORE_INGINNINGS = "highScoreInginnings.js";
-    public static final String BEST_BATTING = "bestBatting.js";
-    public static final String STRIKE_RATE = "strikeRate.js";
-    public static final String BEST_BOWLING = "bestBowling.js";
-    public static final String BEST_BOWLING_INGINNINGS = "bestBowlingInginnings.js";
-    public static final String MOST_HATRICKS = "mostHatricks.js";
-    public static final String BEST_BOWLINNG_AVERAGES = "bestBowlingAverage.js";
-    public static final String MOST_RUNS_OVER = "mostRunsOver.js";
-    public static final String MOST_WICKETS = "mostWickets.js";
-    public static final String MOST_RUNS_CONCEDED = "mostRunsConcededOver.js";
 
-//Images    http://static2.iplt20.com/players/284/135.png
-//CarrerStatus    http://datacdn.iplt20.com/dynamic/data/core/cricket/careerStats/1_careerStats.js
-//Commentry    https://cricketapi.platform.iplt20.com//fixtures/7898/commentary/feeds/1?customer=bcci
-//Match images https://api.platform.iplt20.com/content/ipl/photo/EN/?references=CRICKET_MATCH:7898&tags=gallery-photo&pageSize=150
 
 }
 
