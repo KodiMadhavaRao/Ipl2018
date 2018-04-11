289eb02aae7c6d9c7a52f8863b353374e356e846
Droid <mailtosonumadhav@gmail.com>
2018-04-11 18:52:53 +0530

    Changes

---
+++ app/src/main/java/com/madhav/com/ipl2018/di/qualifier/CommentryQualifier.java
@@ -0,0 +1,16 @@
+package com.madhav.com.ipl2018.di.qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by madhav on 4/11/2018.
+ */
+
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CommentryQualifier {
+}
